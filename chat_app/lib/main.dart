1b3ef5fedef824a22a3bcae5570c7c02596de4a5
Ulbossya <ulbosyn.sayakbay@jihc.kz>
2024-02-01 13:39:46 +0600

    ContactUi

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -19,7 +19,13 @@ class ChatListScreen extends StatefulWidget {
 }
 
 class _ChatListScreenState extends State<ChatListScreen> {
-  List<String> contacts = List.generate(10, (index) => 'Contact $index');
+  List<String> contacts = [
+    'Виктор Власов',
+    'Саша Алексеев',
+    'Пётр Жаринов',
+    'Алина Жукова',
+  ];
+
   List<String> displayedContacts = [];
 
   @override
@@ -28,52 +34,100 @@ class _ChatListScreenState extends State<ChatListScreen> {
     displayedContacts = List.from(contacts);
   }
 
-  void filterContacts(String query) {
-    setState(() {
-      displayedContacts = contacts
-          .where((contact) => contact.toLowerCase().contains(query.toLowerCase()))
-          .toList();
-      displayedContacts = displayedContacts.take(9).toList(); 
-    });
+  String getInitials(String name) {
+    List<String> names = name.split(" ");
+    String initials = "";
+    for (var i = 0; i < names.length; i++) {
+      initials += names[i][0].toUpperCase();
+    }
+    return initials;
+  }
+
+  Color getAvatarBackgroundColor(String contactName) {
+    switch (contactName) {
+      case 'Виктор Власов':
+        return Color.fromRGBO(31, 219, 95, 1);
+      case 'Саша Алексеев':
+        return Color.fromRGBO(246, 103, 0, 1);
+      case 'Пётр Жаринов':
+        return Color.fromRGBO(0, 172, 246, 1);
+      case 'Алина Жукова':
+        return Color.fromRGBO(246, 103, 0, 1);
+      default:
+        return Colors.blue;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat App'),
+        title: Text(
+          'Чаты',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+          ),
+        ),
       ),
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: TextField(
               onChanged: filterContacts,
+              style: TextStyle(color: Color.fromRGBO(157, 183, 203, 1)),
               decoration: InputDecoration(
-                hintText: 'Search...',
-                prefixIcon: Icon(Icons.search),
+                hintText: 'Поиск...',
+                hintStyle: TextStyle(color: Color.fromRGBO(157, 183, 203, 1)),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Color.fromRGBO(157, 183, 203, 1),
+                ),
+                fillColor: Color.fromRGBO(237, 242, 246, 1),
+                filled: true,
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide.none,
                 ),
               ),
             ),
           ),
+         Divider(), 
+
           Expanded(
             child: ListView.builder(
               itemCount: displayedContacts.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage('https://placekitten.com/50/50'),
-                  ),
-                  title: Text(displayedContacts[index]),
-                  subtitle: Text('Last message...'),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ChatScreen(contactName: displayedContacts[index])),
-                    );
-                  },
+                return Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor:
+                            getAvatarBackgroundColor(displayedContacts[index]),
+                        child: Text(
+                          getInitials(displayedContacts[index]),
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      title: Text(displayedContacts[index]),
+                      subtitle: Text('Last message...'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChatScreen(
+                              contactName: displayedContacts[index],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    Divider(), // Add a divider after each contact
+                  ],
                 );
               },
             ),
@@ -81,6 +135,16 @@ class _ChatListScreenState extends State<ChatListScreen> {
         ],
       ),
     );
+  }
+
+  void filterContacts(String query) {
+    setState(() {
+      displayedContacts = contacts
+          .where((contact) =>
+              contact.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+      displayedContacts = displayedContacts.take(4).toList();
+    });
   }
 }
 
@@ -119,8 +183,7 @@ class ChatScreen extends StatelessWidget {
                 ),
                 IconButton(
                   icon: Icon(Icons.send),
-                  onPressed: () {
-                  },
+                  onPressed: () {},
                 ),
               ],
             ),
